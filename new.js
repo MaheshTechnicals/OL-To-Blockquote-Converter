2c2760f4c5d0f97bb7614a48a4edcaa9ef0953fb
msvarma10001@gmail.com <msvarma10001@gmail.com>
2023-04-17 14:31:40 +0530

    Remove duplicate Shayaris

--- new.js
+++ new.js
@@ -1,17 +1,36 @@
 btn.addEventListener("click", () => {
 
-
+  let obj = []
   let allli = document.querySelectorAll("li")
 
 
   const myArray = Array.from(allli).map(item => item.textContent);
 
   myArray.forEach((one) => {
-
+    obj.push(one)
     let b = `<blockquote class="mycard">${one}</blockquote>`
 
-    output.value += b
+
+
   })
+
+
+  let uniqueArr = obj.filter((name, index) => {
+    return obj.indexOf(name) === index;
+  });
+
+
+
+
+  uniqueArr.forEach((every) => {
+
+
+    let c = `<blockquote class="mycard">${every}</blockquote>`
+
+    output.value += c
+  })
+
+
 
 
 })
